e91d29c28dde38f12e90d60b6417586d65431196
Rojan Rajbhandari <rrojan20@gmail.com>
2024-03-08 10:54:58 +0545

    feat: add keymaps for moving lines & buffer navigation

--- lua/config/keymaps.lua
+++ lua/config/keymaps.lua
@@ -20,7 +20,19 @@ keymap("n", "Y", "yy", { noremap = true, silent = true, desc = "Yank whole line"
 -- Easy buffer navigation
 keymap("n", ";;", "<cmd>b#<cr>", { desc = "Switch to last buffer" })
 keymap("n", ";e", "<cmd>Telescope buffers<cr>", { desc = "Buffer explorer" })
-keymap("n", ";d", "<cmd>bd#<cr>", { desc = "Delete current buffer" })
+keymap("n", ";b", "<cmd>BufferLinePick<cr>", { desc = "Buffer Line Pick" })
+keymap("n", ";q", "<cmd>BufferLinePickClose<cr>", { desc = "Buffer Pick and Quit" })
 
 -- Inc rename
-keymap("n", "<leader>rn", ":IncRename ")
+keymap("n", "leader>rn", ":IncRename ")
+
+-- Move lines up & down !
+---- Normal mode
+keymap("n", "<A-j>", ":m .+1<CR>==")
+keymap("n", "<A-k>", ":m .-2<CR>==")
+---- Insert mode
+keymap("i", "<A-j>", "<ESC>:m .+1<CR>==gi")
+keymap("i", "<A-k>", "<ESC>:m .-2<CR>==gi")
+---- Visual mode
+keymap("v", "<A-j>", ":m '>+1<CR>gv=gv")
+keymap("v", "<A-k>", ":m '<-2<CR>gv=gv")

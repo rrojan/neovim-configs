495225024335f90af0656adcfd434d21e294e4be
Rojan Rajbhandari <rrojan20@gmail.com>
2024-03-08 05:56:57 +0545

    feat: configure theme for transparency

---
+++ lua/plugins/theme.lua
@@ -0,0 +1,12 @@
+return {
+  "folke/tokyonight.nvim",
+  opts = {
+    style = "moon",
+    transparent = true,
+    styles = {
+      sidebars = "transparent",
+      floats = "transparent",
+    },
+    sidebars = { "qf", "help" },
+  },
+}

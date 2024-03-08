e14a0cc42a29a534271f7a16aa91272cfbea6995
Rojan Rajbhandari <rrojan20@gmail.com>
2024-03-08 05:54:38 +0545

    feat: extend default LazyVim confs

---
+++ lua/plugins/nvim-treesitter.lua
@@ -0,0 +1,17 @@
+return {
+  "nvim-treesitter/nvim-treesitter",
+  opts = {
+    ensure_installed = {
+      "lua",
+      "javascript",
+      "typescript",
+      "tsx",
+      "json",
+      "html",
+      "css",
+      "scss",
+      "sql",
+      "c",
+    },
+  },
+}
